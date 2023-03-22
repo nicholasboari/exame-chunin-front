b0eab7257fc3d84e19c51299f61c91ef807abccd
nicholasboari <nicholasboari@gmail.com>
2023-03-22 00:48:26 -0300

    create navbar initial

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { Navbar } from "./components/Navbar";
+import "./Global.module.css";
+
+function App() {
+  return <Navbar />;
+}
+
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import styles from "./Navbar.module.css";
+
+export function Navbar() {
+  return (
+    <nav>
+      <div className={styles.container}>
+        <div className={styles["logo-nav-container"]}>
+          <h1 className={styles.h1}>
+            <p>
+              <span>Samurai</span>
+              <span>Car</span>
+            </p>
+          </h1>
+          <div className={styles["navbar-collapse"]}>
+            <ul className={styles["navbar-menu"]}>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+          <div className={styles["login-country-container"]}>
+            <button>Login</button>
+            <img
+              src="https://logodownload.org/wp-content/uploads/2022/05/brazil-flag-bandeira.png"
+              alt="logo-brazil"
+            />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
